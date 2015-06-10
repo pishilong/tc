8b3a10f2c9d9cf230eefff0db29f06368e3df1f8
pishilong <pishilong@gmail.com>
2015-06-10 11:28:17 +0800

    把原始的数据集合转换成weka需要的结构

--- src/Util.java
+++ src/Util.java
@@ -1,10 +1,35 @@
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.channels.*;
 /**
  * Created by pishilong on 15/6/10.
  */
 public class Util {
+    public static void fileCopy(File s, File t) {
+        FileInputStream fi = null;
+        FileOutputStream fo = null;
+        FileChannel in = null;
+        FileChannel out = null;
+        try {
+            fi = new FileInputStream(s);
+            fo = new FileOutputStream(t);
+            in = fi.getChannel();//得到对应的文件通道
+            out = fo.getChannel();//得到对应的文件通道
+            in.transferTo(0, in.size(), out);//连接两个通道，并且从in通道读取，然后写入out通道
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fi.close();
+                in.close();
+                fo.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     /*
     * 读取label文件
     * Map<DocumentID, LabelID>
@@ -15,11 +40,9 @@ public class Util {
         String tempString = null;
         try {
             while ((tempString = reader.readLine()) != null) {
-                // 显示行号
                 Integer docID = Integer.parseInt(tempString.split("\t")[0]);
                 Integer labelID = Integer.parseInt(tempString.split("\t")[1]);
                 labelInfo.put(docID, labelID);
-                System.out.println("docID: " + docID +  "\t labelID:" + labelID);
             }
             reader.close();
 
@@ -36,16 +59,18 @@ public class Util {
         return labelInfo;
     }
 
-    public static void refactorDataDirector(File rawDirectory) throws FileNotFoundException{
+    public static void refactorDataDirector(File rawDirectory) throws Exception{
         String directoryName = rawDirectory.getName();
         String datasetPath = "/Users/pishilong/Workspace/tc/dataset/";
         Map<Integer, Integer> labelInfo;
 
         File resultDirectory = new File(datasetPath + directoryName + "_weka");
+
         if(resultDirectory.exists()){
-            System.out.print("文件夹已转换");
-            return ;
+            resultDirectory.delete();
         }
+        resultDirectory.mkdir();
+
         //读取label文件，获取每个文档的ID，及其对应的类型
         File labelFile = new File(datasetPath + directoryName + ".doc.label");
         if(!labelFile.exists()){
@@ -55,10 +80,28 @@ public class Util {
 
         labelInfo = loadLabelFile(labelFile);
 
+        File[] dataFiles = rawDirectory.listFiles();
+        for (File file : dataFiles) {
+            String fileName = file.getName();
+            Integer docID = Integer.parseInt(fileName);
+            Integer labelID = labelInfo.get(docID);
+            File subDir = new File(resultDirectory.getAbsolutePath() + "/" + labelID);
+            if(!subDir.exists()){
+                subDir.mkdir();
+            }
+            File newFile = new File(subDir.getAbsolutePath() + "/" + fileName);
+            newFile.createNewFile();
+            fileCopy(file, newFile);
+        }
+
     }
 
     public static void main(String[] args) throws Exception {
-        File labelFile = new File("/Users/pishilong/Workspace/tc/dataset/train.doc.label");
-        loadLabelFile(labelFile);
+        // 测试loadLableFile
+        //File labelFile = new File("/Users/pishilong/Workspace/tc/dataset/train.doc.label");
+        //loadLabelFile(labelFile);
+        // 测试refactorDataDirector
+        File trainDirector = new File("/Users/pishilong/Workspace/tc/dataset/train");
+        refactorDataDirector(trainDirector);
     }
 }
